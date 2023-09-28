e5e86f0f3c5bf0f08d1191d6fdfc5b3ed352b241
systemshock89 <666qqq77>
2023-09-28 17:53:51 +0300

    EmployeesListItem add props: name, salary, increase

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,14 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App(){
+
+  // данные "приходящие" с сервера
+  const data = [
+    {name: 'John C.', salary: 800, increase: false},
+    {name: 'Alex M.', salary: 3000, increase: true},
+    {name: 'Carl W.', salary: 5000, increase: false},
+  ];
+
   return (
     <div className="app">
       <AppInfo></AppInfo>
@@ -16,7 +24,7 @@ function App(){
         <AppFilter/>
       </div>
 
-      <EmployeesList/>
+      <EmployeesList data={data}/>
       <EmployeesAddForm/>
 
     </div>
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,22 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    // переберем массив data и возвратим новый
+    const elements = data.map(item => {
+        return (
+            // упрощенный код, где развернем с помощью object spread operator
+            <EmployeesListItem {...item}/>
+            
+            // код, где непосредственно выводятся name, salary
+            // <EmployeesListItem name={item.name} salary={item.salary}/>
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
