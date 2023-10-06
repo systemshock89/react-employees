d8e0f6d73d32ba4d3f0e2661b924e899c035bf65
systemshock89 <666qqq77>
2023-10-06 21:32:40 +0300

    add class EmployeesAddForm with method onValueChange

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,58 @@
+import { Component } from 'react';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+class EmployeesAddForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+    // при вводе текста в input меняем его значение
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value // квадратные скобки используются в ES6 синтаксисе для названия составного св-ва
+        })
+    }
+
+    render(){
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+
+                    {/* в input запускается событие onChange, запускающее метод onValueChange(), в нем setState изменяет состояние
+                    и записывает состояние в this.state.
+                    Далее setState вызывает метод render(), чтобы перерендерить состояние компонениа
+                    и в value записывается актуальное знач-е компонента.
+
+                    Значение формы input'а в этом случае будет контролироваться реактом и сам элемент будет называться управляемым компонентом (элементом)
+                     */}
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?" 
+                        name="name"
+                        value={name} // нужен для создаения управляемого компонента
+                        onChange={this.onValueChange}/>
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $?" 
+                        name="salary"
+                        value={salary} 
+                        onChange={this.onValueChange}/>
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployeesAddForm;
